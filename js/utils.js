65dd571d00df02608b9a9d6f3c1ca51f0eb78c9a
Werner Sosa <2024161143@ub.edu.bz>
2026-03-23 14:06:37 -0600

    Changed classes to fuctions, and started banner and poster action.

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,21 @@
-export function buildHighlightedTitle(title, query) {
+export function highlightTitle(title, query) {
     const container = document.createElement('span');
 
-    const idx = title.toLowerCase().indexOf(query.toLowerCase());
+    const index = title.toLowerCase().indexOf(query.toLowerCase());
 
-    if (idx === -1) {
+    if (index === -1) {
         container.textContent = title;
         return container;
     }
 
-    const before = document.createTextNode(title.slice(0, idx));
+    const before = document.createTextNode(title.slice(0, index));
 
     const match = document.createElement('span');
     match.className = 'highlight';
-    match.textContent = title.slice(idx, idx + query.length);
+    match.textContent = title.slice(index, index + query.length);
 
     const after = document.createTextNode(
-        title.slice(idx + query.length)
+        title.slice(index + query.length)
     );
 
     container.appendChild(before);
@@ -23,4 +23,9 @@ export function buildHighlightedTitle(title, query) {
     container.appendChild(after);
 
     return container;
+}
+
+export function getImageUrl(path, size = "w200") {
+    if (!path) return "";
+    return `https://image.tmdb.org/t/p/${size}${path}`;
 }
